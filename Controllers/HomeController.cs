d4eabf643849b847a3970cab5650a3786a761ca4
kavyajadav <kjadhav@stevens.edu>
2023-11-09 00:18:56 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("about-company")]
+        public IActionResult About()
+        {
+            return View();
+        }
+        [Route("contact-support")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
+
+//Views -> Home -> Index.cshtml
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+        [Route("products")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("search-products/{ProductID?}")]
+        public IActionResult Search(int? ProductID)
+        {
+            ViewBag.ProductID = ProductID;
+            return View();
+        }
+        [Route("order-products")]
+        public IActionResult Order()
+        {
+            return View();
+        }
+    }
+}
